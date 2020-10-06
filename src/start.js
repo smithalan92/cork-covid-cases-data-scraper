5e87c9280ae1e585bd1827ffcfb72a9681bacaae
Alan Smith - Server <smithalan2@gmail.com>
2020-10-06 17:15:28 +0000

    Run at half past

--- src/start.js
+++ src/start.js
@@ -2,7 +2,7 @@ const { CronJob } = require('cron');
 const run = require('./run');
 
 const job = new CronJob({
-  cronTime: '0 15 17 * * *', // UTC time used as server this runs on is UTC
+  cronTime: '0 30 17 * * *', // UTC time used as server this runs on is UTC
   onTick: run,
 });
 
