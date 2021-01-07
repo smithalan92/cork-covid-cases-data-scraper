9aa1e54ccf5930d1c7331349154052c517f07460
Alan Smith - Server <smithalan2@gmail.com>
2021-01-07 18:25:37 +0000

    Run at 7pm

--- src/start.js
+++ src/start.js
@@ -2,7 +2,7 @@ const { CronJob } = require('cron');
 const run = require('./run');
 
 const job = new CronJob({
-  cronTime: '0 30 18 * * *', // UTC time used as server this runs on is UTC
+  cronTime: '0 00 19 * * *', // UTC time used as server this runs on is UTC
   onTick: run,
 });
 
